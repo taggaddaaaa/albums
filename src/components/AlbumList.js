f9224ea374aac52dfabc9a5fe8ea2492e2a56bb8
taggaddaaaa <scaizerg@student.42.fr>
2017-09-07 01:27:33 +0200

    fetch data from heroku server

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 // Import libraries for making a component
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 
 // Make a component
@@ -9,7 +10,12 @@ class AlbumList extends Component {
     // I CAN USE CHROME TO DEBUG THE APP
     // console.log('componentWillMount in AlbumList');
     // debugger;
-  }
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => response.json())
+      .then((responseData) => this.setState({ albums: responseData }));
+    console.log(this);
+
+}
 
 
   render() {
