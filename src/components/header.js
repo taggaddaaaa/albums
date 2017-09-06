c08c9a112686f76e31bc475885ddc7d36bff967f
taggaddaaaa <scaizerg@student.42.fr>
2017-09-06 21:38:27 +0200

    Make a header component that can be used everywhere in the app

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
 // TODO: Import libraries for making a component
-
+import React from 'react';
+import { Text } from 'react-native';
 
 
 // TODO: Make a component
-
-
+const Header = () => {
+  return <Text>Albums</Text>;
+};
 
 
 //TODO: Make the component available to other parts of the app
-
+export default Header;
 
