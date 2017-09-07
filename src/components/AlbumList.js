9b7f90cff432275dbcb439484ec6ddd23bc0c5bd
taggaddaaaa <sabinecaizergues@gmail.com>
2017-09-07 17:24:18 +0200

    init state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -6,6 +6,10 @@ import axios from 'axios';
 
 // Make a component
 class AlbumList extends Component {
+  state= { 
+    albums: [] 
+  };
+
   componentWillMount() {
     // I CAN USE CHROME TO DEBUG THE APP
     // console.log('componentWillMount in AlbumList');
@@ -13,12 +17,13 @@ class AlbumList extends Component {
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
       .then((response) => response.json())
       .then((responseData) => this.setState({ albums: responseData }));
-    console.log(this);
+    console.log('objet that is returned by fetch method: ', this);
 
 }
 
 
   render() {
+    console.log('state of albumList : ', this.state);
     return (
       <View>
         <Text>
