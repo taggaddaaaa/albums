b1c9c8e343bee27a8da38a9017e0dae3e6011ab2
taggaddaaaa <scaizerg@student.42.fr>
2017-09-09 19:08:37 +0200

    Style the button and use a react native component that shows ti the user that the button is clicked on click use 'onPress' to log a message on click Next work will be to make this onPress function reusable with diverse others functions

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 
 const Button = () => {
+  const {buttonStyle, textStyle } = styles;
+
   return (
-    <Text>Click Me !</Text>
+  <TouchableOpacity onPress={() => console.log('pressed!')} style={buttonStyle}>
+    <Text style={textStyle}>Click Me !</Text>
+  </TouchableOpacity>
   );
+};
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#E87975',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingBottom: 10,
+    paddingTop: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#E87975',
+    marginLeft: 5,
+    marginRight: 5
+  }
 };
 
 
